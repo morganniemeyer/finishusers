15ddcff63383db01b96b3539367333714905bac2
Morgan Niemeyer <malineniemeyer@gmail.com>
2022-09-27 13:43:56 -0500

    complete stretch goals

--- users/users.js
+++ users/users.js
@@ -1,5 +1,6 @@
 // this will: check we have a user, set signout link, fetch user profile
 import '../auth/user.js';
+import { getProfiles, getUser } from '../fetch-utils.js';
 // > Part D import getUser and getProfiles from fetch-utils
 import { renderProfile } from '../render-utils.js';
 
@@ -10,6 +11,9 @@ let profiles = [];
 
 window.addEventListener('load', async () => {
     // > Part D: await getProfiles and assign error and profiles state
+    const response = await getProfiles();
+    error = response.error;
+    profiles = response.data;
 
     if (error) {
         // eslint-disable-next-line no-console
@@ -23,12 +27,14 @@ window.addEventListener('load', async () => {
 
 async function displayProfiles() {
     // > Part D: get the current user
-    const user = null; // ???
+    const user = getUser(); // ???
 
     for (const profile of profiles) {
         // > Part D:
         //      - call renderProfile passing in the profile and
         //        the current user's id:
         //      - append the rendered el to the list:
+        const profileEl = renderProfile(profile, user.id);
+        profileList.append(profileEl);
     }
 }
